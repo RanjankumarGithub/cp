427f2683df4b39f73e515d78a39a168028cc278f
RANJAN KUMAR <104021055+RanjankumarGithub@users.noreply.github.com>
2024-09-02 19:45:25 +0530

    problem - 231A - codeforces pushed

---
+++ 231A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int cnt = 0;
+    
+    while(n--){
+        int a, b, c;
+        cin>>a>>b>>c;
+        if((a == 1 && b==1) || (b == 1 && c == 1) || ( a == 1 && c ==1)) cnt++;
+    }
+    cout<<cnt<<endl;
+}
